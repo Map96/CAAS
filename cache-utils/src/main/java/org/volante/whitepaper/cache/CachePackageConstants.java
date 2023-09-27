d0aa55618ce332f61974db012161eda1fff5ac77
tanay.agarwal <tanay.agarwal@volantetech.com>
2023-09-27 22:57:26 +0530

    Read From Env Null Check

--- cache-utils/src/main/java/org/volante/whitepaper/cache/CachePackageConstants.java
+++ cache-utils/src/main/java/org/volante/whitepaper/cache/CachePackageConstants.java
@@ -1,16 +1,16 @@
 package org.volante.whitepaper.cache;
 
 public class CachePackageConstants {
-    public static final String MONGODB_HOST ;
+    public static final String MONGODB_HOST;
     public static final String REDIS_HOST;
-    public static final String KAFKA_HOST ;
-    public static final int MONGODB_PORT ;
-    public static final int REDIS_PORT ;
-    public static final int KAFKA_PORT ;
+    public static final String KAFKA_HOST;
+    public static final int MONGODB_PORT;
+    public static final int REDIS_PORT;
+    public static final int KAFKA_PORT;
     public static final int TTL;
-    public static final int NUM_THREADS ;
-    public static final int NUM_QUERIES_PER_THREAD ;
-    public static final boolean isExternalCache ;
+    public static final int NUM_THREADS;
+    public static final int NUM_QUERIES_PER_THREAD;
+    public static final boolean isExternalCache;
     public static final String CACHE_DB = "cacheDb";
     public static final String CHARACTER_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String OBJECT_ID = "objectId";
@@ -31,19 +31,19 @@ public class CachePackageConstants {
     public static final String CACHE_DATA_JOB = "CacheDataJob";
     public static final String CACHE_DATA_TRIGGER = "CacheDataTrigger";
     public static final String CACHE_DATA_GROUP = "CacheDataGroup";
-    public static final int SCHEDULE_TIME ;
+    public static final int SCHEDULE_TIME;
 
-    static{
-        MONGODB_HOST = System.getenv("MONGODB_HOST");
-        REDIS_HOST = System.getenv("REDIS_HOST");
-        KAFKA_HOST = System.getenv("KAFKA_HOST");
-        MONGODB_PORT = Integer.parseInt(System.getenv("MONGODB_PORT"));
-        REDIS_PORT = Integer.parseInt(System.getenv("REDIS_PORT"));
-        KAFKA_PORT = Integer.parseInt(System.getenv("KAFKA_PORT"));
-        NUM_THREADS = Integer.parseInt(System.getenv("NUM_THREADS"));
-        NUM_QUERIES_PER_THREAD = Integer.parseInt(System.getenv("NUM_QUERIES_PER_THREAD"));
-        isExternalCache = Boolean.parseBoolean(System.getenv("true"));
-        SCHEDULE_TIME = Integer.parseInt(System.getenv("SCHEDULE_TIME"));
-        TTL = Integer.parseInt(System.getenv("TTL"));
+    static {
+        MONGODB_HOST = System.getenv("MONGODB_HOST") != null ? System.getenv("MONGODB_HOST") : "localhost";
+        REDIS_HOST = System.getenv("REDIS_HOST") != null ? System.getenv("REDIS_HOST") : "localhost";
+        KAFKA_HOST = System.getenv("KAFKA_HOST") != null ? System.getenv("KAFKA_HOST") : "localhost";
+        MONGODB_PORT = System.getenv("MONGODB_PORT") != null ? Integer.parseInt(System.getenv("MONGODB_PORT")) : 27017;
+        REDIS_PORT = System.getenv("REDIS_PORT") != null ? Integer.parseInt(System.getenv("REDIS_PORT")) : 6379;
+        KAFKA_PORT = System.getenv("KAFKA_PORT") != null ? Integer.parseInt(System.getenv("KAFKA_PORT")) : 9092;
+        SCHEDULE_TIME = System.getenv("SCHEDULE_TIME") != null ? Integer.parseInt(System.getenv("SCHEDULE_TIME")) : 1;
+        TTL = System.getenv("TTL") != null ? Integer.parseInt(System.getenv("TTL")) : 10;
+        NUM_THREADS = System.getenv("NUM_THREADS") != null ? Integer.parseInt(System.getenv("NUM_THREADS")) : 10;
+        NUM_QUERIES_PER_THREAD = System.getenv("NUM_QUERIES_PER_THREAD") != null ? Integer.parseInt(System.getenv("NUM_QUERIES_PER_THREAD")) : 10000;
+        isExternalCache = System.getenv("isExternalCache") == null || Boolean.parseBoolean(System.getenv("isExternalCache"));
     }
 }
